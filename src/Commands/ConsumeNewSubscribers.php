3a46d7b9e2bdd47f9e84edfd25b8050a6084f8ce
Connor Graham <connor.graham@enta.net>
2019-02-07 13:37:49 +0000

    Create basic command

--- src/Commands/ConsumeNewSubscribers.php
+++ src/Commands/ConsumeNewSubscribers.php
@@ -1,13 +1,35 @@
 <?php
 namespace App\Commands;
 
+use App\Services\ClockworkService;
+use App\Services\KafkaService;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ConsumeNewSubscribers
 {
+    /**
+     * @var KafkaService
+     */
+    private $kafkaService;
+
+    /**
+     * @var ClockworkService
+     */
+    private $clockworkService;
+
+    public function __construct(KafkaService $kafkaService, ClockworkService $clockworkService)
+    {
+        $this->kafkaService = $kafkaService;
+        $this->clockworkService = $clockworkService;
+    }
+
     public function __invoke(OutputInterface $output)
     {
-        // TODO Connect to kafka topic here!
         $output->writeln("Consuming events!");
+
+        $this->kafkaService->consume('cat_fact_subscribe', function ($payload) use ($output) {
+            $output->writeln('Sending text');
+            $this->clockworkService->sendText($payload['number'], 'You are now subscribed to cat facts');
+        });
     }
 }
