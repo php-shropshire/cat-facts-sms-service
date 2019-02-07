84ede766ec86af901d12f76a2accc3c6e5f4f50f
Connor Graham <connor.graham@enta.net>
2019-02-07 15:34:39 +0000

    Implement clockwork service

--- src/Services/ClockworkService.php
+++ src/Services/ClockworkService.php
@@ -5,11 +5,29 @@ namespace App\Services;
 
 class ClockworkService
 {
-  // TODO Implement clockwork sms integration
 
-    public function sendText($number,$text)
+    private $clockwork;
+
+    public function __construct()
     {
+        $this->clockwork = new \mediaburst\ClockworkSMS\Clockwork( getenv('CLOCKWORK_API_KEY') );
+    }
 
+    public function sendText($number, $text)
+    {
+        try {
+            $message = [
+                'to' => $number,
+                'message' => $text,
+                'from' => 'CatFacts'
+            ];
+
+            $result = $this->clockwork->send($message);
+
+            return $result['success'];
+        } catch (ClockworkException $e) {
+            return false;
+        }
     }
 
 }
