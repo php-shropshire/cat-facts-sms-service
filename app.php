eb9b3387f135e92cb5234be3b1a2b0d9e7b5fff5
Connor Graham <connor.graham@enta.net>
2019-02-07 15:33:28 +0000

    Load .env

--- app.php
+++ app.php
@@ -2,6 +2,9 @@
 
 include "vendor/autoload.php";
 
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
 $app = new Silly\Edition\PhpDi\Application();
 
 $app->command('consume:subscribers', '\App\Commands\ConsumeNewSubscribers');
